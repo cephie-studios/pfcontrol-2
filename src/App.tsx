e450628b629b5816f8ce541eed88e34a523a5295
AviationYYZ <charlie.yablon@icloud.com>
2025-10-08 15:57:06 -0400

    Made ACARS tester only

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,10 @@ export default function App() {
                                         element={<Logbook />}
                                     />
                                     <Route
+                                        path="acars/:sessionId"
+                                        element={<ACARS />}
+                                    />
+                                    <Route
                                         path="logbook/:flightId"
                                         element={<FlightDetail />}
                                     />
@@ -76,7 +80,6 @@ export default function App() {
 
                     <Route path="/login" element={<Login />} />
                     <Route path="/submit/:sessionId" element={<Submit />} />
-                    <Route path="acars/:sessionId" element={<ACARS />} />
                     <Route
                         path="/flight/:shareToken"
                         element={<PublicFlightView />}
