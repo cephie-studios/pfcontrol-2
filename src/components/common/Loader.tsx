9cc78ca92773b24f97f4baed8a8eb810c334c70e
Formic Acid <formicacidgd@gmail.com>
2025-10-14 20:51:46 +0200

    Codebase Cleanup

--- src/components/common/Loader.tsx
+++
@@ -1,6 +0,0 @@
-// src/components/common/Loader.tsx
-import { Loader2 } from "lucide-react";
-
-export default function Loader() {
-   return <Loader2 className="animate-spin h-8 w-8 text-blue-400" />;
-}
