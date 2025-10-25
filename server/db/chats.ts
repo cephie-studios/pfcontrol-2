9b4562746849396c3e7dcae38ddf93d3d58601ad
dev-banane <jakob@uplift.de>
2025-10-25 15:51:54 +0200

    mention fix

--- server/db/chats.ts
+++ server/db/chats.ts
@@ -45,7 +45,7 @@ export async function addChatMessage(sessionId: string, { userId, username, avat
             username,
             avatar,
             message: JSON.stringify(encryptedMsg),
-            mentions: mentions.length > 0 ? sql`${JSON.stringify(mentions)}::jsonb` : undefined,
+            mentions: mentions.length > 0 ? sql`ARRAY[${mentions.map(m => sql`${m}`).join(', ')}]` : sql`'{}'::text[]`,
         })
         .returningAll()
         .executeTakeFirst();
