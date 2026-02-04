6f2014796a0674af10d8dfb33a5a96b860840999
CoolerMinecraft <Simon.BrawlStars@web.de>
2026-02-04 19:07:29 +0100

    updated cors to fit new domain

--- server/main.ts
+++ server/main.ts
@@ -66,8 +66,8 @@ app.use(
     origin:
       process.env.NODE_ENV === 'production'
         ? [
-            'https://control.pfconnect.online',
-            'https://canary.pfconnect.online',
+            'https://pfcontrol.com',
+            'https://canary.pcontrol.com',
           ]
         : ['http://localhost:9901', 'http://localhost:5173'],
     credentials: true,
