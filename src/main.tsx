3cc9bfa0a07f5fce734f27299e46b500fb10b816
dev-banane <jakob@uplift.de>
2026-05-09 00:46:19 +0200

    feat: session replays

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-
+import { PostHogProvider } from '@posthog/react';
 import { AuthProvider } from './hooks/auth/AuthProvider.tsx';
 import { DataProvider } from './hooks/data/DataProvider.tsx';
 import { SettingsProvider } from './hooks/settings/SettingsProvider.tsx';
 
+const posthogOptions = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
+  defaults: '2026-01-30',
+  persistence: 'memory' as const,
+} as const;
+
 createRoot(document.getElementById('root')!).render(
-  <AuthProvider>
-    <DataProvider>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
-    </DataProvider>
-  </AuthProvider>
+  <PostHogProvider
+    apiKey={import.meta.env.VITE_POSTHOG_KEY}
+    options={posthogOptions}
+  >
+    <AuthProvider>
+      <DataProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </DataProvider>
+    </AuthProvider>
+  </PostHogProvider>
 );
