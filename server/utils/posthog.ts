562c9ddc6fd91397b6913341ccf78277b59c2a03
dev-banane <jakob@uplift.de>
2026-05-08 23:51:22 +0200

    fix: tests
    Co-Authored-By: CJ <cj@1ceit.com>

--- server/utils/posthog.ts
+++ server/utils/posthog.ts
@@ -1,15 +1,14 @@
 import { PostHog } from 'posthog-node';
 
-const client = new PostHog(process.env.POSTHOG_API_KEY!, {
-  host: process.env.POSTHOG_HOST,
-});
+const noop = { capture: () => {}, shutdown: async () => {} } as unknown as PostHog;
 
-process.on('SIGINT', async () => {
-  await client.shutdown();
-});
+const client: PostHog = process.env.POSTHOG_API_KEY
+  ? new PostHog(process.env.POSTHOG_API_KEY, { host: process.env.POSTHOG_HOST })
+  : noop;
 
-process.on('SIGTERM', async () => {
-  await client.shutdown();
-});
+if (process.env.POSTHOG_API_KEY) {
+  process.on('SIGINT', async () => { await client.shutdown(); });
+  process.on('SIGTERM', async () => { await client.shutdown(); });
+}
 
 export default client;
