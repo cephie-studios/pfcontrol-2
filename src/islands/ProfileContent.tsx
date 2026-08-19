b06e1da30fa0e1ede42fdb550279f0e432de4bf7
CJ <cj@1ceit.com>
2026-08-19 02:03:57 -0400

    fix: astro ssr

--- src/islands/ProfileContent.tsx
+++ src/islands/ProfileContent.tsx
@@ -1,6 +1,7 @@
 import './loadIslandStyles';
 import { AuthProvider } from '../hooks/auth/AuthProvider';
 import { DataProvider } from '../hooks/data/DataProvider';
+import { ToastProvider } from '../components/common/ToastProvider';
 import Navbar from '../components/Navbar';
 import AppOverlays from '../components/AppOverlays';
 import PilotProfile from '../pages/PilotProfile';
@@ -15,15 +16,17 @@ interface Props {
 export default function ProfileContent({ username, pathname }: Props) {
   return (
     <PostHogProviderWrapper>
-      <AuthProvider>
-        <DataProvider>
-          <IsomorphicRouter pathname={pathname}>
-            <Navbar />
-            <AppOverlays />
-            <PilotProfile standalone={false} usernameOverride={username} />
-          </IsomorphicRouter>
-        </DataProvider>
-      </AuthProvider>
+      <ToastProvider>
+        <AuthProvider>
+          <DataProvider>
+            <IsomorphicRouter pathname={pathname}>
+              <Navbar />
+              <AppOverlays />
+              <PilotProfile standalone={false} usernameOverride={username} />
+            </IsomorphicRouter>
+          </DataProvider>
+        </AuthProvider>
+      </ToastProvider>
     </PostHogProviderWrapper>
   );
 }
